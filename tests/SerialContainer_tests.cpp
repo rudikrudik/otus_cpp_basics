e0a89b534a6b4afb024e53bf65039fac2def2b4a
rudikrudik <rudik.login@gmail.com>
2023-12-07 12:08:59 +0300

    Add tests

--- tests/SerialContainer_tests.cpp
+++ tests/SerialContainer_tests.cpp
@@ -1,3 +1,18 @@
-//
-// Created by user on 05.12.2023.
-//
+#include <gtest/gtest.h>
+#include "../SerialContainer.hpp"
+
+TEST(TestGroupName, Subtest_1) {
+    ASSERT_TRUE(1 == 1);
+}
+
+TEST(TestGroupName, Subtest_2) {
+    ASSERT_FALSE('b' == 'b');
+    std::cout << "continue test after failure" << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
